ded83a50ccb869208c04322292316c25bcb09bf5
ggg71 <ggg7152@gmail.com>
2022-01-06 17:26:37 +0900

    learn data binding {}

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,22 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let posts = {color: 'skyblue', fontSize: '30px'};
+  function 함수() {
+    return 100
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style = {posts}>개발 Blog</div>
+      </div>
+      <img src= { logo } />
+      <h4>{ 함수() }</h4>
     </div>
   );
 }
