b4e91ea177b50f031ee7a0aa48f5916b8139a064
ggg71 <ggg7152@gmail.com>
2022-01-06 18:42:35 +0900

    learn how to use state in react 3 (immutable data)

--- blog/src/App.js
+++ blog/src/App.js
@@ -11,12 +11,25 @@ function App() {
   let [글제목,글제목변경] = useState(['리액트 공부','자바스크립트 공부','노드js 공부']); 
   let [따봉, 따봉변경] = useState(0);
 
+  function 제목바꾸기() {
+    // <Array,Object state 데이터 수정 방법 > : immutable data-리액트 원칙
+    // 
+	  // - 1. 기존 state 카피본 만들기
+	  // - 2. 카피본에 수정사항 반영
+	  // - 3. 변경함수()에 집어넣기
+    //
+    var newArray = [...글제목]; //1.(deep copy를 사용) state의 복사본을 만든다.
+    newArray[0] = '여자 코트 추천'; //2.0번째 인덱스의 내용을 바꾼다.
+    글제목변경(newArray);//3.변경함수에 저장
+  }
+
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ () => { 글제목변경(['여자코트추천',글제목[1],글제목[2]]) }}>버튼</button>
+      <button onClick={ 제목바꾸기 }>버튼</button>
       <div className="list">
         <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1);}}>👍</span> {따봉} </h3>
         <p> 1월 05일 발행</p>
