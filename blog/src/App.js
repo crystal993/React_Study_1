f3e97e3a3875a21f4bcbbd88011ba6612fe9e1f0
ggg71 <ggg7152@gmail.com>
2022-01-06 18:17:22 +0900

    learn how to use state in react

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,15 +9,16 @@ function App() {
   // var [a,b] = [10, 100];
   // a = 10, b = 100 이렇게 저장됨
   let [글제목,글제목변경] = useState(['리액트 공부','자바스크립트 공부','노드js 공부']); 
-  let [글제목2,글제목변경2] = useState('여자 코트 추천2'); 
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
+      <button onClick={ () => { 글제목변경(['여자코트추천','여자코트추천2','여자코트추천3']) }}>버튼</button>
       <div className="list">
-        <h3> { 글제목[0] } </h3>
+        <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1);}}>👍</span> {따봉} </h3>
         <p> 1월 05일 발행</p>
         <hr/>
       </div>
