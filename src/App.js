0327129fc2a35c105cf897fa2e33c1bc0eb8dca0
ggg71 <ggg7152@gmail.com>
2022-01-13 21:01:54 +0900

    모달창 클릭하면 나타나는 기능 -state변수 이용

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ function App() {
 
   let [글제목,글제목변경] = useState(['리액트 공부','자바스크립트 공부','노드js 공부']); 
   let [따봉, 따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false);
 
-  function 제목바꾸기() {
-    var newArray = [...글제목]; //1.(deep copy를 사용) state의 복사본을 만든다.
-    newArray[0] = '여자 코트 추천'; //2.0번째 인덱스의 내용을 바꾼다.
-    글제목변경(newArray.sort());//3.변경함수에 저장
-  }
+  // function 제목바꾸기() {
+  //   var newArray = [...글제목]; //1.(deep copy를 사용) state의 복사본을 만든다.
+  //   newArray[0] = '여자 코트 추천'; //2.0번째 인덱스의 내용을 바꾼다.
+  //   글제목변경(newArray.sort());//3.변경함수에 저장
+  // }
 
 
   return (
@@ -20,7 +21,7 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <button onClick={ 제목바꾸기 }>버튼</button>
+      {/* <button onClick={ 제목바꾸기 }>버튼</button> */}
       <div className="list">
         <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉+1);}}>👍</span> {따봉} </h3>
         <p> 1월 05일 발행</p>
@@ -32,12 +33,17 @@ function App() {
         <hr/>
       </div>
       <div className="list">
-        <h3> { 글제목[2] } </h3>
+        <h3 onClick={()=>{modal변경(true)}}> { 글제목[2] } </h3>
         <p> 1월 07일 발행</p>
         <hr/>
       </div>
 
-      <Modal></Modal>
+      {
+        modal === true
+        ? <Modal></Modal>
+        : null // 텅 빈 html이라는 뜻
+      }
+       
 
     </div>
   );
